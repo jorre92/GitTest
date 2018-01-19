3c7980133bd194215ab11cca7c0a65c49f9688e2
Johannes Eriksson Wallin <johanneserikssonwallin@Johanness-MacBook-Pro.local>
2018-01-19 15:44:23 +0100

    New: You can add humans to the db. You can fetch all entities in the data base. Req: SQLite3 to be installed on the mac.

--- Entity Framework on Mac/DBModels/DBModels.cs
+++ Entity Framework on Mac/DBModels/DBModels.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace EntityFrameworkonMac
 {
-    public class human
+    public class Human
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public String FirstName { get; set; }
+        public String SirName { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Human> Humans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=HumanRegister.db");
+        }
     }
 
 }
--- Entity Framework on Mac/Program.cs
+++ Entity Framework on Mac/Program.cs
@@ -1,13 +1,84 @@
 ﻿using System;
 using System.Linq;
 
-namespace EntityFrameworkOnMac
+
+namespace EntityFrameworkonMac
 {
     class Program
-    {
+    {   
+        const String CloseApp = "close";
+        const String AddPerson = "add";
+        const String ShowDatabase = "list";
+
         static void Main(string[] args)
         {
             
+            using ( var dbContext = new DatabaseContext())
+            {
+                dbContext.Database.EnsureCreated();
+
+                while(true)
+                {
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine(CloseApp + " - Exit program.");
+                    Console.WriteLine(AddPerson + " - Add new human.");
+                    Console.WriteLine(ShowDatabase + " - Show human register!");
+
+                    String input = Console.ReadLine();
+
+                    switch(input)
+                    {
+                        case CloseApp:
+                            return;
+                        case AddPerson:
+                            AddPeronFunc(dbContext);
+                            break;
+                        case ShowDatabase:
+                            ShowDBContent(dbContext);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            }
+        }
+
+
+        public  static void AddPeronFunc(DatabaseContext ctxt)
+        {
+            Console.Write("Enter humans first name : ");
+            String firstName = Console.ReadLine();
+            Console.Write("Enter humans sir name :");
+            String sirName = Console.ReadLine();
+            Console.Write("Eanter humans birt year (int) :");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.Write("Eanter humans birt month (int) :");
+
+            int month = Int32.Parse(Console.ReadLine());
+            Console.Write("Eanter humans birt day (int) :");
+            int day = Int32.Parse(Console.ReadLine());
+
+            Human newHuman = new Human()
+            {
+                FirstName = firstName,
+                SirName = sirName,
+                BirthDay = new DateTime(year, month, day)
+            };
+
+            ctxt.Humans.Add(newHuman);
+
+            ctxt.SaveChanges();
+        }
+    
+        public static void ShowDBContent(DatabaseContext ctxt)
+        {
+            var humans = from h in ctxt.Humans orderby h.SirName select h;
+ 
+            foreach(Human h in ctxt.Humans)
+            {
+                Console.WriteLine($"{h.SirName} {h.FirstName}, {h.BirthDay}.");
+            }
         }
     }
 }
